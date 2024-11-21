d684246a4a7e0020ccb60b784b364a087bf5bfc3
DavidSooler <00001126699834sp@al.educacao.sp.gov.br>
2024-11-21 16:18:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que são placas tectônicas?',
+    'As placas tectônicas são grandes porções da litosfera terrestre que se movem sobre o manto, causando fenômenos como terremotos e a formação de montanhas.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a Lei da Inércia?',
+    'A Lei da Inércia, formulada por Isaac Newton, afirma que um corpo em repouso ou em movimento permanecerá nesse estado até que uma força externa atue sobre ele.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a importância da água na química?',
+    'A água é um solvente universal essencial em inúmeras reações químicas, sendo vital para processos biológicos e industriais.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Napoleão Bonaparte foi um líder militar e político francês que se destacou durante a Revolução Francesa e se tornou imperador, influenciando profundamente a Europa no início do século XIX.'
+)
